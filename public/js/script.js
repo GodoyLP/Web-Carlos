fca996de27be5d692f021b1dbf32cb135632636a
GodoyLP <126925948+GodoyLP@users.noreply.github.com>
2024-11-15 14:08:23 -0300

    Delete public/js directory
    eliimino carpte js de public

--- public/js/script.js
+++
@@ -1,9 +0,0 @@
-
-/*boton whatsapp abrir en la web*/
-function whatsapp() {
-    Object.assign(document.createElement("a"), {
-      target: "_blank",
-      href: "https://wa.me/+541122224444"
-    }).click();
-  };
-  
